b19bc11f381194821d959b9d1cd918105d048531
AlfieDarko <me@alfiedarko.co.uk>
2020-07-02 18:28:46 +0100

    added navigation navbar component

---
+++ src/views/navbar/components/NavBar.jsx
@@ -0,0 +1,45 @@
+import React, { PureComponent } from "react";
+import { locales } from "../../lib/locales";
+import "./styles.css";
+
+const NavBar = () => {
+  const language = locales["NAVBAR"];
+  return (
+    <nav className="navbar">
+      <ul className="navbar__ul">
+        <li className="navbar__item">
+          <a
+            className="navbar__anchor-tag"
+            data-qa="navbar-home-link"
+            href={language.HOME.URL}
+          >
+            {language.HOME.TEXT}
+          </a>
+        </li>
+
+        <li className="navbar__item">
+          <a
+            className="navbar__anchor-tag"
+            data-qa="navbar-comments-link"
+            href={language.COMMENTS.URL}
+          >
+            {language.COMMENTS.TEXT}
+          </a>
+        </li>
+
+        <li className="navbar__item">
+          <a
+            data-qa="navbar-github-link"
+            className="navbar__anchor-tag"
+            href={language.GITHUB_REPO.URL}
+            target="_blank"
+          >
+            {language.GITHUB_REPO.TEXT}
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
