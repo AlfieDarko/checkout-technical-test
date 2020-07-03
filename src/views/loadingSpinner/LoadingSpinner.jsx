60dd2e4999c6bafd64707a83d271ce5767ab6988
AlfieDarko <me@alfiedarko.co.uk>
2020-07-03 12:15:20 +0100

    added CSS loading spinner

---
+++ src/views/loadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./loading-spinner.css";
+
+const LoadingSpinner = () => {
+  return <div data-testid="loading-spinner" className="loading-spinner"></div>;
+};
+
+export default LoadingSpinner;
