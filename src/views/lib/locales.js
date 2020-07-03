f5f2ae680c4633bf922a2e592ed58183862e995d
AlfieDarko <me@alfiedarko.co.uk>
2020-07-03 12:16:18 +0100

    Created NavBar Component

---
+++ src/views/lib/locales.js
@@ -0,0 +1,24 @@
+export const locales = {
+  NAVBAR: {
+    HOME: {
+      TEXT: "Home",
+      URL: "/",
+    },
+    LINKEDIN: {
+      TEXT: "LinkedIn",
+      URL: "https://www.linkedin.com/in/alfie-darko/",
+    },
+    COMMENTS: {
+      TEXT: "Comments",
+      URL: "/#comments",
+    },
+    GITHUB_REPO: {
+      TEXT: "Github Repo",
+      URL: "https://github.com/AlfieDarko/checkout-technical-test",
+    },
+    ALFIEDARKO_SITE: {
+      TEXT: "alfiedarko.co.uk",
+      URL: "https://alfiedarko.co.uk",
+    },
+  },
+};
--- src/views/navbar/components/NavBar.jsx
+++ src/views/navbar/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import { locales } from "../../lib/locales";
 import "./styles.css";
 
