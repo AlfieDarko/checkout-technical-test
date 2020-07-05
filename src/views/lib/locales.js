a5bc3298dd7ff7c72ed1f0113ab0db8d73ded129
AlfieDarko <me@alfiedarko.co.uk>
2020-07-05 19:47:34 +0100

    fix(locales) added the rest of the locale files

--- src/views/lib/locales.js
+++ src/views/lib/locales.js
@@ -13,12 +13,50 @@ export const locales = {
       URL: "/#comments",
     },
     GITHUB_REPO: {
-      TEXT: "Github Repo",
+      TEXT: "Repo",
       URL: "https://github.com/AlfieDarko/checkout-technical-test",
     },
     ALFIEDARKO_SITE: {
       TEXT: "alfiedarko.co.uk",
       URL: "https://alfiedarko.co.uk",
     },
+  },
+  POST_FEEDBACK: {
+    COMMENT: {
+      FORM_LABEL: "Comment*",
+      REQUIRED_TEXT: "Don't forget to leave a comment!",
+      PLACEHOLDER: "Leave a comment",
+      INVALID: "Please leave a comment",
+    },
+    EMAIL: {
+      FORM_LABEL: "Email*",
+      REQUIRED_TEXT: "Don't forget to enter your email address!",
+      PLACEHOLDER: "Enter your email",
+      INVALID: "Invalid email address",
+    },
+    NAME: {
+      FORM_LABEL: "Name*",
+      REQUIRED_TEXT: "Don't forget to enter your name!",
+      PLACEHOLDER: "Please enter your name",
+    },
+    RATING: {
+      FORM_LABEL: "Rating*",
+      REQUIRED_TEXT: "Don't forget to leave a rating",
+      MESSAGE: "Please leave a rating",
+    },
+    FORM_LEGEND: "Tell the world how you feel about Pizza!",
+    SUBMIT_CTA: {
+      SUBMIT_COMMENT: "Submit Comment",
+      SUBMITTED: "Submitted",
+    },
+    FORM_ASYNC_STATUS: {
+      SUCCESS: "Your feedback was received! Thank you.",
+      FAILURE: "Something went wrong. Please try again",
+    },
+  },
+  PIZZA_VIEW: {
+    IMAGE_TITLE: "Pepperoni Pizza",
+    DESCRIPTION:
+      "Lovingly made with a traditional pizza base covered in home made passata topped with mozzarella parmesan sliced pepperoni rocket basil and sliced jalapeno.",
   },
 };
