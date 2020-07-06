9f8d1ee338552c6fb631ef31cfe72aeb57ddf03d
AlfieDarko <me@alfiedarko.co.uk>
2020-07-06 18:06:02 +0100

    changed file location to alias

--- src/views/pizzaSection/components/imageCard/index.jsx
+++ src/views/pizzaSection/components/imageCard/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import PizzaSrc from "../../../assets/optimized-pizza.jpeg";
+import PizzaSrc from "#assets/optimized-pizza.jpeg";
 
 import { locales } from "#views/lib/locales";
 import "./styles.css";
