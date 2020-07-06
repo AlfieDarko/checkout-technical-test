b50051a31cb8e3eb98fedbffd922ce5414d76070
AlfieDarko <me@alfiedarko.co.uk>
2020-07-06 15:51:14 +0100

    changed loading spinner folder formats

--- src/views/loadingSpinner/LoadingSpinner.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./loading-spinner.css";
-
-const LoadingSpinner = () => {
-  return <div data-testid="loading-spinner" className="loading-spinner"></div>;
-};
-
-export default LoadingSpinner;
---
+++ src/views/loadingSpinner/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+const LoadingSpinner = () => (
+  <div data-testid="loading-spinner" className="loading-spinner"></div>
+);
+
+export default LoadingSpinner;
